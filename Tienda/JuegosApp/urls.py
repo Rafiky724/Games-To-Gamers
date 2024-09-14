0ad16e78c2593769e75782c7a18beec7701b0c8e
Rafiky724 <jhonangelfuentesperez83@gmail.com>
2024-09-14 18:44:46 -0500

    Crud de juegos funcionando

--- Tienda/JuegosApp/urls.py
+++ Tienda/JuegosApp/urls.py
@@ -1,11 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name=''),
-    path('control/', views.controlGame, name='control'),
-    # path('registrarCurso/', views.registrarCurso),
-    # path('edicionCurso/<codigo>', views.edicionCurso),
-    # path('editarCurso/', views.editarCurso),
-    # path('eliminacionCurso/<codigo>', views.eliminarCurso),
-]
+    path('control/', views.controlGame, name = 'control'),
+    path('registerGame/', views.registerGame, name = 'register'),
+    path('editGame/<codigo>/', views.editGame, name = 'edit'),
+    path('gameEdition/', views.gameEdition, name = 'game'),
+    path('deleteGame/<codigo>/', views.deleteGame, name = 'delete'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Tienda/JuegosApp/views.py
+++ Tienda/JuegosApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Game
 
 # Create your views here.
@@ -10,3 +10,51 @@ def controlGame(request):
     games_list = Game.objects.all()
     return render(request, 'controlGame/control.html', {'games': games_list})
 
+def registerGame(request):
+    if request.method == 'POST':
+        codigo = request.POST['txtCodigo']
+        nombre = request.POST['txtNombre']
+        descripcion = request.POST['txtDescripcion']
+        precio = request.POST['numPrecio']
+        plataforma = request.POST['txtPlataforma']
+        imagenes = request.FILES.get('imgImagen')
+        
+        game = Game.objects.create(codigo=codigo, nombre=nombre, descripcion=descripcion, precio=precio, plataforma=plataforma, imagenes=imagenes)
+        return redirect('control')
+    else:
+        return redirect('control')
+
+def editGame(request, codigo):
+    game = get_object_or_404(Game, codigo=codigo)
+    return render(request, 'controlGame/editGame.html', {'game': game})
+
+def gameEdition(request):
+    if request.method == 'POST':
+        codigo = request.POST['txtCodigo']
+        nombre = request.POST['txtNombre']
+        descripcion = request.POST['txtDescripcion']
+        precio = request.POST['numPrecio']
+        plataforma = request.POST['txtPlataforma']
+        imagenes = request.FILES.get('imgImagen', None)
+        
+        if ',' in precio:
+            precio = precio.replace(',', '.')
+            
+        game = get_object_or_404(Game, codigo=codigo)
+        game.nombre = nombre
+        game.descripcion = descripcion
+        game.precio = precio
+        game.plataforma = plataforma
+        if imagenes:
+            game.imagenes = imagenes
+        game.save()
+        
+        return redirect('control')
+    else:
+        return redirect('control')
+
+
+def deleteGame(request, codigo):
+    game = get_object_or_404(Game, codigo=codigo)
+    game.delete()
+    return redirect('control')
